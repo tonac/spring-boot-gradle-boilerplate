6585691cd54a11efed2f92f84612321e472d19a1
Bipin Thite <bipin.thite@aptitudelabs.com>
2019-01-06 19:37:41 +0530

    Initial commit :tada:

---
+++ src/main/java/com/example/app/service/AppService.java
@@ -0,0 +1,23 @@
+package com.example.app.service;
+
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author https://github.com/ibipn
+ *
+ */
+@Service
+@Slf4j
+public class AppService {
+
+  public String sayHello() {
+    log.trace("Enter: sayHello()");
+    boolean dummyError = System.currentTimeMillis() % 2 == 0;
+    if (dummyError) {
+      throw new RuntimeException("Something went wrong!");
+    }
+    return "Hello World!";
+  }
+
+}
